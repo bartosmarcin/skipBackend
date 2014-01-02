b48bb16439e1b86aaaa672725d420c304dedd035
bartosmarcin <bartosmarcin@gmail.com>
2014-01-02 23:21:34 +0100

    Added drivers manager test class

---
+++ src/main/java/skip/DriversManagerTest.java
@@ -0,0 +1,36 @@
+package skip;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DriversManagerTest {
+	DriversManager mgr = new DriversManager();
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAddDriver() {
+		Driver d = new Driver("test","driver");
+		mgr.addDriver(d);
+	}
+
+}
