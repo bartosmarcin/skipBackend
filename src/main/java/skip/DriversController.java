2e275dfd50175f54cbb9d25d9e9ac5da6a586a56
szkopeka <artszko@gmail.com>
2014-01-05 19:24:32 +0100

    Organized imports

--- src/main/java/skip/DriversController.java
+++ src/main/java/skip/DriversController.java
@@ -1,12 +1,10 @@
 package skip;
 
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
