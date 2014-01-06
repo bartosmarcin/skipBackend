0c3bf7f92618ef71f6fb8f2c2926828d9dc3c873
krzeszob <b.krzeszowski@gmail.com>
2014-01-06 06:53:37 +0100

    Updated Application class.
    Added import annotation for the security configuration file.

--- src/main/java/skip/Application.java
+++ src/main/java/skip/Application.java
@@ -3,12 +3,17 @@ package skip;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @ComponentScan
 @EnableAutoConfiguration
+@Configuration
+@ImportResource("classpath*:security/spring-security.xml")
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
 }
