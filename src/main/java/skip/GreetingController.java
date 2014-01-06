8fcedc115544a3a5c30b36bdc18b62192d4e919d
krzeszob <b.krzeszowski@gmail.com>
2014-01-06 06:55:12 +0100

    Updated GreetingController class
    Added simple functionality for testing the implemented security.

--- src/main/java/skip/GreetingController.java
+++ src/main/java/skip/GreetingController.java
@@ -1,5 +1,6 @@
 package skip;
 
+import java.security.Principal;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,4 +19,11 @@ public class GreetingController {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
+    
+    @RequestMapping("/welcome")
+    public @ResponseBody Greeting welcome(Principal principal) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, principal.getName()));
+    }
+    
 }
