429bb40e0bf4b6dd6ef7059f0b3d8216ef6eb9b8
bartosmarcin <bartosmarcin@gmail.com>
2014-01-25 17:33:49 +0100

    Changed mapping to more common

--- src/main/java/skip/DriversController.java
+++ src/main/java/skip/DriversController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -18,20 +20,20 @@ public class DriversController {
 		return dmgr.getDriverById(id);
 	}
 	
-	@RequestMapping(value="/")
+	@RequestMapping(value="/", method=RequestMethod.GET)
 	public @ResponseBody List<Driver> getDriver(){
 		return dmgr.getDriversList();
 	}
 	
-	@RequestMapping(value="/remove/{id}")
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	public @ResponseBody Driver removeDriver(
 				@PathVariable("id") Long id){
 		return dmgr.removeDriver(id);
 	}
 	
-	@RequestMapping(value="/add/{json}")
+	@RequestMapping(value="/", method=RequestMethod.POST)
 	public @ResponseBody Driver addDriver(
-				@PathVariable("json") String json){
+				@RequestBody(required=true) String json){
 		return dmgr.addDriver(json);
 	}
 	
