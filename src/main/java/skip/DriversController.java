55839e706069e6673b08db9d5b66089586f7944a
bartosmarcin <bartosmarcin@gmail.com>
2014-05-12 00:07:35 +0200

    Moved to heroku db

--- src/main/java/skip/DriversController.java
+++ src/main/java/skip/DriversController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.hibernate.dialect.PostgreSQLDialect;;;
 @Controller
 public class DriversController {
 	DriversManager dmgr = new DriversManager();
