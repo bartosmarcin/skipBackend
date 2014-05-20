18a78d0ff660c1dc8d7c06a45520189fbf1d5516
krzeszob <b.krzeszowski@gmail.com>
2014-05-20 23:58:47 +0200

    Added 2 driver accounts

--- src/main/resources/security/db-input.sql
+++ src/main/resources/security/db-input.sql
@@ -1,7 +1,11 @@
 insert into users (username, password, enabled) values
 ('admin', 'admin$p', true),
-('user', 'user$p', true);
+('user', 'user$p', true),
+('driver1', '1234', true),
+('driver2', '4321', true);
 
 insert into authorities (username, authority) values
 ('admin', 'ROLE_ADMIN'),
-('user', 'ROLE_USER');
+('user', 'ROLE_USER'),
+('driver1', 'ROLE_USER'),
+('driver2', 'ROLE_USER');
